86cedb990e48133b39606b2dcfdfc316b449b92c
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-02-08 12:14:36 +0100

    Testing MessageContext on Login.js

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,24 @@
+import { useEffect } from "react";
+import useMessage from "../../context/Message/MessageContext.js";
+
 function Login() {
+	/* ---- States ---------------------------------- */
+	const messages = useMessage();
+
+	/* ---- Effects --------------------------------- */
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			messages.setLimit(4);
+			messages.add("success", "Dommage, ça marche");
+			messages.add("info", "Dommage, c'est une information");
+			messages.add("warning", "Dommage, c'est un avertissement");
+			messages.add("error", "Dommage, ça marche pas");
+		}, 1000);
+		return () => clearTimeout(timer);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	/* ---- Page content ---------------------------- */
 	return (
 		<>
 			<h1>Connexion</h1>
