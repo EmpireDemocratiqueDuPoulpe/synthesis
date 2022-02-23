fa4787551164db2f3bfbac6cc29bc34b08720ac6
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-02-23 22:26:49 +0100

    Moved React.StrictMode inside the AuthProvider

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
 import { MessageProvider } from "./context/Message/MessageContext.js";
+import { AuthProvider } from "./context/Auth/AuthContext.js";
 import App from "./pages/App/App.js";
 // import reportWebVitals from "./reportWebVitals.js";
 import "normalize.css";
 import "./index.css";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Router>
+	<Router>
+		<CookiesProvider>
 			<MessageProvider>
-				<App/>
+				<AuthProvider>
+					<React.StrictMode>
+						<App/>
+					</React.StrictMode>
+				</AuthProvider>
 			</MessageProvider>
-		</Router>
-	</React.StrictMode>,
+		</CookiesProvider>
+	</Router>,
 	document.getElementById("root")
 );
 
