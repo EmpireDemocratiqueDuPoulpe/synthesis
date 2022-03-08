12fb98b7c1aef1b8f4589f9424fe35f346649cc8
Breadator258 <maxence.pawlowski@supinfo.com>
2022-03-08 16:48:41 +0100

    Added "ects" for "module"

--- src/pages/Notes/Notes.js
+++ src/pages/Notes/Notes.js
@@ -1,25 +1,38 @@
 import { useState } from "react";
+import useAuth from "../../context/Auth/AuthContext.js";
 import Collapsible from "../../components/Collapsible/Collapsible.js";
 import "./Notes.css";
 
 function Notes() {
 	/* ---- States ---------------------------------- */
-	const firstName = "Jean-Michel";
-	const lastName = "Eudes";
-	const campus = "Isle McDonald";
+	const { user } = useAuth();
+
 	const [notes] = useState([
-		{moduleName: "1WORK", note: 10.5},
-		{moduleName: "1TEAM", note: 15.0},
-		{moduleName: "2JAVA", note: 16.25}
+		{moduleName: "1WORK", note: 10.5, year: 1},
+		{moduleName: "1TEAM", note: 15.0, year: 1},
+		{moduleName: "2JAVA", note: 16.25, year: 2}
 	]);
+
+	//const [totalEcts, setTotalEcts] = useState(65);
+
+	const [selectedYear, setSelectedYear] = useState(4);
+
 
 	/* ---- Page content ---------------------------- */
 	return (
 		<>
-			<h2>Notes de {firstName} {lastName}</h2>
-			<h3>Campus de {campus}</h3>
+			<h2>Notes de {user.first_name} {user.last_name}</h2>
+			<h3>Campus de {user.campus}</h3>
 			<div>
-				{notes.map((module, index) => {
+				<select value={selectedYear} onChange={(e) => setSelectedYear(parseInt(e.target.value, 10))}>
+					<option value="1">A.Sc.1</option>
+					<option value="2">A.Sc.2</option>
+					<option value="3">B.Sc.1</option>
+					<option value="4">M.Eng.1</option>
+					<option value="5">M.Eng.2</option>
+				</select>
+
+				{notes.filter((m) => m.year === selectedYear).map((module, index) => {
 					return (
 						<Collapsible key={`module-${index}-note`} title={module.moduleName}>
 							<p>{module.note}</p>
@@ -27,6 +40,7 @@ function Notes() {
 					);
 				})}
 			</div>
+			<p>ECTS totaux : {notes.filter((m) => m.year === selectedYear).reduce((acc, note) => acc + note.note, 0)}/60</p>
 		</>
 	);
 }
