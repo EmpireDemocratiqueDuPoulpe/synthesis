30819b6339fc056dcb1d6470704e4ec2c03d0ff9
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-04-13 15:09:16 +0200

    Installed React Query

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { CookiesProvider } from "react-cookie";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { MessageProvider } from "./context/Message/MessageContext.js";
 import { AuthProvider } from "./context/Auth/AuthContext.js";
 import App from "./pages/App/App.js";
@@ -9,14 +11,20 @@ import App from "./pages/App/App.js";
 import "normalize.css";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
 	<Router>
 		<CookiesProvider>
 			<MessageProvider>
 				<AuthProvider>
-					<React.StrictMode>
-						<App/>
-					</React.StrictMode>
+					<QueryClientProvider client={queryClient}>
+						<React.StrictMode>
+							<App/>
+						</React.StrictMode>
+
+						<ReactQueryDevtools initialIsOpen={false} toggleButtonProps={{ style: {opacity: 0.5} }}/>
+					</QueryClientProvider>
 				</AuthProvider>
 			</MessageProvider>
 		</CookiesProvider>
