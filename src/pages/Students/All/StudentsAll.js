6ea049dcfcbb7549e048def1a93f8a8c4d4909b0
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-05-06 08:12:32 +0200

    Possible to filter student by (almost) all properties

--- src/pages/Students/All/StudentsAll.js
+++ src/pages/Students/All/StudentsAll.js
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import useAuth from "../../../context/Auth/AuthContext.js";
 import Loader from "../../../components/Loader/Loader.js";
 import useStudents from "../../../hooks/students/useStudents.js";
+import { sortObjectArr } from "../../../global/Functions.js";
 
 function StudentsAll() {
 	/* ---- States ---------------------------------- */
 	const { permissions } = useAuth();
+	const [sortBy, setSortBy] = useState("first_name");
 	const students = useStudents({
 		expand: [
 			(permissions.READ_CAMPUS ? "campus" : ""), (permissions.READ_MODULES ? "module" : ""),
@@ -13,10 +16,25 @@ function StudentsAll() {
 		].filter(Boolean)
 	});
 	
+	const handleSortChange = event => {
+		setSortBy(event.target.value);
+	};
+	
 	/* ---- Page content ---------------------------- */
 	return (
 		<div className="Students StudentsAll">
 			<Link to="/">&lt;-- Retour</Link>
+			
+			<select value={sortBy} onChange={handleSortChange}>
+				<option value="first_name">Pr&eacute;nom</option>
+				<option value="last_name">Nom</option>
+				<option value="email">Adresse email</option>
+				<option value="birthdate" disabled>Date de naissance</option>
+				<option value="study.current_level">Niveau actuel</option>
+				<option value="campus.name">Campus</option>
+				<option value="region">R&eacute;gion</option>
+				<option value="null" disabled>Modules</option>
+			</select>
 			
 			{!students.isUsable() ? (students.isLoading && <Loader/>) : (
 				<table>
@@ -28,13 +46,13 @@ function StudentsAll() {
 							<th>Date de naissance</th>
 							<th>Niveau actuel</th>
 							{permissions.READ_CAMPUS && <th>Campus</th>}
-							<th>Region</th>
+							<th>R&eacute;gion</th>
 							{permissions.READ_MODULES && <th>Modules</th>}
 						</tr>
 					</thead>
 					
 					<tbody>
-						{students.data.map(student => (
+						{students.data.sort((a, b) => sortObjectArr(sortBy, a, b)).map(student => (
 							<tr key={`students-list-student-${student.user_id}`}>
 								<td className="student-first-name">{student.first_name}</td>
 								<td className="student-last-name">{student.last_name}</td>
