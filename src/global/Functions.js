19c2cfec3209b04d1dacaa6ff15aa2e218d87518
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-05-06 08:11:06 +0200

    Add sortObjectArr function. Used to sort an array of object by a common property

--- src/global/Functions.js
+++ src/global/Functions.js
@@ -10,6 +10,50 @@ export function clickOnEnter(e, callback, ...callbackArgs) {
 }
 
 /*****************************************************
+ * Types
+ *****************************************************/
+
+/** @see https://stackoverflow.com/a/28475765 */
+function typeOf(obj) {
+	return {}.toString.call(obj).split(" ")[1].slice(0, -1).toLowerCase();
+}
+
+/*****************************************************
+ * Arrays
+ *****************************************************/
+
+import { get } from "lodash-es";
+
+export function sortObjectArr(propPath, obj1, obj2) {
+	const val1 = get(obj1, propPath);
+	const val2 = get(obj2, propPath);
+	
+	if (!val1 && val2) return 1;
+	if (!val2 && val1) return -1;
+	
+	switch (typeOf(val1)) {
+		case "string":
+			return sortStr(val1, val2);
+		case "number":
+			return sortNumber(val1, val2);
+		case "boolean":
+			return sortBool(val1, val2);
+	}
+}
+
+function sortStr(str1, str2) {
+	return str1.localeCompare(str2);
+}
+
+function sortNumber(num1, num2) {
+	return num1 - num2;
+}
+
+function sortBool(bool1, bool2) {
+	return (bool1 === bool2) ? 0 : (bool1 ? -1 : 1);
+}
+
+/*****************************************************
  * Date
  *****************************************************/
 
