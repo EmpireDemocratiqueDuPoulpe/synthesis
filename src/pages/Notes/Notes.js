cfa439bd15bf44a907c3ace5caff04ca039eea3e
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-05-08 13:31:33 +0200

    Fix loop issue

--- src/pages/Notes/Notes.js
+++ src/pages/Notes/Notes.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import Select from "react-select";
 import useAuth from "../../context/Auth/AuthContext.js";
 import useNotesOfUser from "../../hooks/notes/useNotesOfUser.js";
@@ -6,7 +7,6 @@ import useStudies from "../../hooks/studies/useStudies.js";
 import Collapsible from "../../components/Collapsible/Collapsible.js";
 import Loader from "../../components/Loader/Loader.js";
 import "./Notes.css";
-import {Link} from "react-router-dom";
 
 function Notes() {
 	/* ---- States ---------------------------------- */
@@ -20,7 +20,6 @@ function Notes() {
 		{ value: 4, label: "M.Eng.1" },
 		{ value: 5, label: "M.Eng.2" }
 	];
-
 	const [selectedYears, setSelectedYears] = useState([yearsOptions[3]]);
 	const notes = useNotesOfUser({ userID: user.user_id, years: selectedYears.map(y => y.value) });
 
@@ -30,6 +29,7 @@ function Notes() {
 			<Link to="/">&lt;-- Retour</Link>
 			<h2>Notes de {user.first_name} {user.last_name}</h2>
 			<h3>Campus de {user.campus.name}</h3>
+			
 			<div>
 				{(!notes.isUsable() || !study.isUsable()) ? ((notes.isLoading || study.isLoading) && <Loader/>) : (
 					<>
@@ -38,13 +38,14 @@ function Notes() {
 							defaultValue={selectedYears}
 							onChange={setSelectedYears}
 							isMulti/>
-						{notes.data.map((module, mIndex) => {
+						
+						{notes.data.map(module => {
 							ects.total += module.ects;
 							ects.current += (module.notes.reduce((acc, note) => acc + note.note, 0) / module.notes.length) >= 10 ? module.ects : 0;
 							return (
-								<Collapsible key={`module-${mIndex}`} title={module.year + module.name}>
-									{module.notes.map((note, nIndex) => (
-										<p key={`module-${mIndex}-note-${nIndex}`}>{note.note}</p>
+								<Collapsible key={`notes-list-module-${module.module_id}`} title={module.year + module.name}>
+									{module.notes.map(note => (
+										<p key={`notes-list-module-${module.module_id}-note-${note.note_id}`}>{note.note}</p>
 									))}
 								</Collapsible>
 							);
