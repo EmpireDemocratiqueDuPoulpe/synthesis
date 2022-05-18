ac680c3a47ed6ca1cff4b248c0bb6c57d08dac8e
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-05-18 07:38:53 +0200

    Remove unused link

--- src/components/AuthorizedLinks/AuthorizedLinks.js
+++ src/components/AuthorizedLinks/AuthorizedLinks.js
@@ -17,7 +17,6 @@ function AuthorizedLinks() {
 				{hasPermission(permissions.READ_STUDENTS) && (<li><Link to="/students">&Eacute;tudiants</Link></li>)}
 				{hasPermission(permissions.READ_SCTS) && (<li><Link to="#">SCTs</Link></li>)}
 				{hasPermission(permissions.READ_PLANNINGS) && (<li><Link to="#">Planning</Link></li>)}
-				{hasPermission(permissions.READ_CAMPUS) && (<li><Link to="#">Campus</Link></li>)}
 				{hasPermission(permissions.READ_MODULES) && (<li><Link to="/modules">Modules</Link></li>)}
 				{hasPermission(permissions.READ_NOTES) && (<li><Link to="/notes">Notes</Link></li>)}
 				{hasPermission(permissions.READ_RESITS) && (<li><Link to="#">Resits</Link></li>)}
