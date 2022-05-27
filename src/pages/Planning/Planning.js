70e4d5bff7fcaaa0d0d0299cacc6b87ba9d9aa02
SumBread <45918820+Breadator258@users.noreply.github.com>
2022-05-27 20:14:11 +0200

    Planning is working (yey, finally, oh hello, hello, i'm a mirror, i am lonely)

--- src/pages/Planning/Planning.js
+++ src/pages/Planning/Planning.js
@@ -5,55 +5,42 @@ import Loader from "../../components/Loader/Loader.js";
 import Kalend, { CalendarView } from "kalend";
 import "kalend/dist/styles/index.css";
 import "./Planning.css";
-import {useState} from "react";
 
 function Planning() {
 	/* ---- States ---------------------------------- */
 	const { user } = useAuth();
-
-	const [planningEvents, setPlanningEvents] = useState([]);
-
-	const planning = usePlanning({year: user.study ? user.study.current_level : "", eventType: "module"}, {
-		onSuccess: data => {
-			let events = [];
-			data.map((event)=> {
-				events.push({
-					startAt: event.date,
-					endAt: event.date,
-					timezoneStartAt: "Europe/Paris", // optional
-					summary: event.event_name,
-					color: "blue",
-					calendarID: "planning",
-					key: event.planning_id
-				});
-			});
-			setPlanningEvents(events);
-			console.log(data);
-		},
-		onError: () => console.log("Error retrieving events"),
-		retry: 0
-	});
+	// TODO: EVENTTYPE ACCEPTE UN TABLEAU
+	const planning = usePlanning({year: user.study ? user.study.current_level : "", eventType: ""});
 
 	/* ---- Page content ---------------------------- */
 	return (
 		<div className="Modules">
 			<Link to="/">&lt;-- Retour</Link>
 			<h2>Planning</h2>
-			<Kalend
-				events={planningEvents}
-				initialDate={new Date().toISOString()}
-				hourHeight={10}
-				initialView={CalendarView.MONTH}
-				disabledViews={[CalendarView.DAY]}
-				timeFormat={"24"}
-				weekDayStart={"Monday"}
-				showWeekNumbers={true}
-				calendarIDsHidden={["planning"]}
-				language={"fr"}
-				draggingDisabledConditions={{calendarID: "planning"}}
-			/>
 			{!planning.isUsable() ? (planning.isLoading && <Loader/>) : (
-				console.log(planning)
+				<Kalend
+					events={planning.data.map((event)=> {
+						return {
+							key: event.planning_id,
+							startAt: event.start_date,
+							endAt: event.end_date,
+							timezoneStartAt: "Europe/Paris", // optional
+							summary: event.event_name,
+							color: "blue",
+							calendarID: "planning"
+						};
+					}).flat(1)}
+					initialDate={new Date().toISOString()}
+					hourHeight={10}
+					initialView={CalendarView.MONTH}
+					disabledViews={[CalendarView.DAY]}
+					timeFormat={"24"}
+					weekDayStart={"Monday"}
+					showWeekNumbers={true}
+					calendarIDsHidden={["planning"]}
+					language={"fr"}
+					draggingDisabledConditions={{calendarID: "planning"}}
+				/>
 			)}
 
 		</div>
