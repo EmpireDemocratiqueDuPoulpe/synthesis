7a216e00d5710868dffda32f3b20115e703158a4
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-06-01 17:55:08 +0200

    Add a filter to get old students

--- src/hooks/students/useStudents.js
+++ src/hooks/students/useStudents.js
@@ -2,17 +2,17 @@ import { useQueryClient, useQuery } from "react-query";
 import useMessage from "../../context/Message/MessageContext.js";
 import { API } from "../../config/config.js";
 
-function useStudents({ UUID, campus, onlyHired, expand }, options = {}) {
+function useStudents({ UUID, campus, onlyHired, onlyOld, expand }, options = {}) {
 	/* ---- Queries --------------------------------- */
 	const messages = useMessage();
 	const queryClient = useQueryClient();
 	const students = useQuery(
-		["students", { UUID, campus, onlyHired, expand }],
+		["students", { UUID, campus, onlyHired, onlyOld, expand }],
 		async () => {
 			if (UUID) {
 				return (await API.students.getByUUID.fetch({ UUID }, { campus, expand })).student;
 			} else {
-				return (await API.students.getAll.fetch(null, { campus, onlyHired, expand })).students;
+				return (await API.students.getAll.fetch(null, { campus, onlyHired, onlyOld, expand })).students;
 			}
 		},
 		{ ...options, onError: err => messages.add(err.type, err, retry) }
