478a6a22405935219a616be1403b73ccba9b7ad5
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-06-06 21:56:33 +0200

    WIP: Job offers style

--- src/pages/JobOffers/All/JobOffersAll.js
+++ src/pages/JobOffers/All/JobOffersAll.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import useJobOffers from "../../../hooks/jobOffers/useJobOffers.js";
 import { isoStrToDate } from "../../../global/Functions.js";
 import Loader from "../../../components/Loader/Loader.js";
+import "./JobOffersAll.css";
 
 function JobOffersAll() {
 	/* ---- States ---------------------------------- */
@@ -15,29 +16,40 @@ function JobOffersAll() {
 			<Link to="/jobs/offers/add">+ Ajouter</Link>
 
 			<label>
-				<input type="checkbox" value={expired} onChange={(e) => setExpired(e.target.checked)}/>
+				<input type="checkbox" value={expired} onChange={(e) => setExpired(e.target.checked)} checked={expired}/>
 				Afficher les offres ayant expir&eacute;es
 			</label>
 
-			{!jobOffers.isUsable() ? (jobOffers.isLoading && <Loader/>) : (jobOffers.data.map((offer) => (
-				<div key={`job-offer-${offer.job_offer_id}`} className="job-offer">
-					<Link to={`/jobs/offer/${offer.job_offer_id}`} style={
-						offer.expiration_date ? (new Date() > isoStrToDate(offer.expiration_date) ? { textDecoration: "line-through" } : null) : null
-					}>
-						<h3>{offer.title}</h3>
-						<p>{offer.type}</p>
-						<p>{offer.company_name}</p>
-						<p>{offer.city}, {offer.postal_code}</p>
-						<p>{offer.expiration_date}</p>
-					</Link>
-
-					<div>
-						{offer.jobDomains.map((domain) => (
-							<span key={`job-offer-${offer.job_offer_id}-domain-${domain.job_domain_id}`}>{domain.name}</span>
-						))}
-					</div>
+			{!jobOffers.isUsable() ? (jobOffers.isLoading && <Loader/>) : (
+				<div className="job-offers">
+					{jobOffers.data.map((offer) => {
+						const expDate = offer.expiration_date ? isoStrToDate(offer.expiration_date) : null;
+						const expired = expDate ? (new Date() > expDate) : false;
+						
+						return (
+							<div key={`job-offer-${offer.job_offer_id}`} className={`job-offer${expired ? " expired" : ""}`}>
+								<Link to={`/jobs/offer/${offer.job_offer_id}`}>
+									<h3 className="job-offer-title">
+										{offer.title}
+										{expDate && <span className="job-offer-exp-date">{expDate.toLocaleDateString()} {expired ? " [EXPIRÉE]" : ""}</span>}
+									</h3>
+									<p className="job-offer-company">{offer.company_name}, {offer.city}, {offer.postal_code}</p>
+									<p className="job-offer-type">{offer.type}</p>
+									
+									<div className="job-offer-domains">
+										{offer.jobDomains.map((domain, index) => (
+											<span key={`job-offer-${offer.job_offer_id}-domain-${domain.job_domain_id}`}>
+												{domain.name}
+												{(index < (offer.jobDomains.length - 1)) ? ", " : ""}
+											</span>
+										))}
+									</div>
+								</Link>
+							</div>
+						);
+					})}
 				</div>
-			)))}
+			)}
 		</div>
 	);
 }
