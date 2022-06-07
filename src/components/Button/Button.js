f72d338df078d8f1407c535dfe152f8206f4010e
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-06-07 16:57:11 +0200

    Add props to Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,33 +2,34 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./Button.css";
 
-function Button({ link, icon, children }) {
-	/* ---- Functions ------------------------------- */
-
-	
+function Button({ onClick, link, icon, color, children }) {
 	/* ---- Page content ---------------------------- */
 	return (
-		<Wrapper link={link} className="button">
+		<Wrapper link={link} className={`button ${color}-color`} onClick={onClick}>
 			{icon && (<span className="button-icon">{icon}</span>)}
 			{children}
 		</Wrapper>
 	);
 }
 Button.propTypes = {
+	onClick: PropTypes.func,
 	link: PropTypes.shape({
 		to: PropTypes.string.isRequired,
 		external: PropTypes.bool /* For some reason, the IDE "thinks" that isRequired means optional. */
 	}),
 	icon: PropTypes.any,
+	color: PropTypes.oneOf([ "primary", "red" ]),
 	children: PropTypes.node
 };
+Button.defaultProps = { color: "primary" };
 
-function Wrapper({ link, children, ...rest }) {
+function Wrapper({ link, onClick, children, ...rest }) {
 	return link
 		? (link.external ? (<a href={link.to} target="_blank" rel="noreferrer" {...rest}>{children}</a>) : (<Link to={link.to} {...rest}>{children}</Link>))
-		: <button {...rest}>{children}</button>;
+		: <button onClick={onClick} {...rest}>{children}</button>;
 }
 Wrapper.propTypes = {
+	onClick: Button.propTypes.onClick,
 	link: Button.propTypes.link,
 	children: PropTypes.node
 };
