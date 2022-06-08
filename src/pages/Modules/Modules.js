c24ab7cb10f26152d7b5df4690a120d223da3f43
SumBread <45918820+Breadator258@users.noreply.github.com>
2022-06-08 18:59:57 +0200

    First module page design

--- src/pages/Modules/Modules.js
+++ src/pages/Modules/Modules.js
@@ -20,7 +20,31 @@ function Modules() {
 		user.study ? yearsOptions.filter(yo => yo.value <= user.study.current_level) : yearsOptions
 	);
 	const modules = useModules({ years: selectedYears.map(y => y.value) });
+	//////////////////////////////////////////
+	/*const elements = document.querySelectorAll(".module");
 
+	window.addEventListener("scroll", function() {
+		elements.forEach(function ( element){
+			var position = element.getBoundingClientRect();
+
+			if(position.top < window.innerHeight && position.bottom >= 0) {
+				if(element.classList.contains("hide_module")){
+					element.classList.remove("hide_module");
+				}
+				if(!element.classList.contains("show_module")){
+					element.classList.add("show_module");
+				}
+			}else{
+				if(!element.classList.contains("hide_module")){
+					element.classList.add("hide_module");
+				}
+				if(element.classList.contains("show_module")){
+					element.classList.remove("show_module");
+				}
+			}
+		});
+	});*/
+	
 	/* ---- Page content ---------------------------- */
 	return (
 		<div className="Modules">
@@ -36,16 +60,20 @@ function Modules() {
 							isMulti/>
 
 					)}
-
-					{modules.data.map(module => {
-						return (
-							<div key={`modules-list-module-${module.module_id}`}>
-								<p>{module.year}{module.name}{(module.users && module.users.length > 0) && (" - " + module.users.map(prof => (
-									`${prof.first_name} ${prof.last_name}`
-								)).join(", "))}</p>
-							</div>
-						);
-					})}
+					<div className="modulesRoot">
+						{modules.data.map(module => {
+							return (
+								<div className="module <!--hide_module-->" key={`modules-list-module-${module.module_id}`}>
+									<h3 className="module_name">{module.year}{module.name}{(module.users && module.users.length > 0)}</h3>
+									<h4 className="module_longname">{module.long_name}</h4>
+									<p>{module.users.map(prof => (
+										`${prof.first_name} ${prof.last_name}`
+									)).join(", ")}</p>
+									<p className="module_description">{module.description}</p>
+								</div>
+							);
+						})}
+					</div>
 				</div>
 			)}
 
