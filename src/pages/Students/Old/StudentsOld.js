541cf6b9adea2d4a23b957a41632abe0baf17001
SumBread <45918820+Breadator258@users.noreply.github.com>
2022-06-16 20:24:01 +0200

    Design for OldStudents

--- src/pages/Students/Old/StudentsOld.js
+++ src/pages/Students/Old/StudentsOld.js
@@ -5,6 +5,7 @@ import useStudents from "../../../hooks/students/useStudents.js";
 import Loader from "../../../components/Loader/Loader.js";
 import SearchBar from "../../../components/SearchBar/SearchBar.js";
 import { sortObjectArr } from "../../../global/Functions.js";
+import Inputs from "../../../components/Inputs/Inputs";
 
 function StudentsOld() {
 	/* ---- States ---------------------------------- */
@@ -24,50 +25,62 @@ function StudentsOld() {
 	/* ---- Page content ---------------------------- */
 	return (
 		<div className="Students StudentsOld">
-			<select value={sortBy} onChange={handleSortChange} disabled={!students.isUsable()}>
-				<option value="first_name">Pr&eacute;nom</option>
-				<option value="last_name">Nom</option>
-				<option value="email">Adresse email</option>
-				<option value="birthdate" disabled>Date de naissance</option>
-				<option value="study.current_level">Niveau actuel</option>
-				{hasPermission(permissions.READ_CAMPUS) && <option value="campus.name">Campus</option>}
-				<option value="region">R&eacute;gion</option>
-			</select>
+			<h2>Liste des anciens étudiants</h2>
+			<Inputs.Select
+				name="studentsSelect"
+				value={sortBy}
+				onChange={handleSortChange}
+				options={[
+					{value:"first_name", label:"Prénom"},
+					{value:"last_name", label:"Nom"},
+					{value:"email", label:"Adresse email"},
+					{value:"birthdate", label:"Date de naissance", disabled: true},
+					{value:"study.current_level", label:"Niveau actuel"},
+					(hasPermission(permissions.READ_CAMPUS) ? ({value: "campus.name", label: "Campus"}) : null),
+					{value:"region", label:"Région"}
+
+				].filter(Boolean)}
+				disabled={!students.isUsable()}
+			>
+				Trier par
+			</Inputs.Select>
 			
 			<SearchBar placeholder="Rechercher" value={search} setValue={setSearch} disabled/>
 			
 			{!students.isUsable() ? (students.isLoading && <Loader/>) : (
-				<table>
-					<thead>
-						<tr>
-							<th>Pr&eacute;nom</th>
-							<th>Nom</th>
-							<th>Adresse e-mail</th>
-							<th>Date de naissance</th>
-							<th>Niveau de sortie</th>
-							<th>Date de sortie</th>
-							{hasPermission(permissions.READ_CAMPUS) && <th>Campus</th>}
-							<th>R&eacute;gion</th>
-							<th>Actions</th>
-						</tr>
-					</thead>
-					
-					<tbody>
-						{students.data.sort((a, b) => sortObjectArr(sortBy, a, b)).map(student => (
-							<tr key={`students-old-list-student-${student.user_id}`}>
-								<td className="student-old-first-name">{student.first_name}</td>
-								<td className="student-old-last-name">{student.last_name}</td>
-								<td className="student-old-email">{student.email}</td>
-								<td className="student-old-birth-date">{student.birth_date}</td>
-								<td className="student-old-exit-level">{student.study.exit_level}</td>
-								<td className="student-old-exit-date">{student.study.exit_date}</td>
-								{hasPermission(permissions.READ_CAMPUS) && <td className="student-old-campus">{student.campus.name}</td>}
-								<td className="student-old-region">{student.region}</td>
-								<td className="student-old-action"><Link to={`/user/${student.uuid}`}>Vers le profil</Link></td>
+				<div className="TableMainWrapper">
+					<table className="TableMain">
+						<thead>
+							<tr>
+								<th>Pr&eacute;nom</th>
+								<th>Nom</th>
+								<th>Adresse e-mail</th>
+								<th>Date de naissance</th>
+								<th>Niveau de sortie</th>
+								<th>Date de sortie</th>
+								{hasPermission(permissions.READ_CAMPUS) && <th>Campus</th>}
+								<th>R&eacute;gion</th>
+								<th>Actions</th>
 							</tr>
-						))}
-					</tbody>
-				</table>
+						</thead>
+
+						<tbody>
+							{students.data.sort((a, b) => sortObjectArr(sortBy, a, b)).map(student => (
+								<tr key={`students-old-list-student-${student.user_id}`}>
+									<td className="student-old-first-name">{student.first_name}</td>
+									<td className="student-old-last-name">{student.last_name}</td>
+									<td className="student-old-email">{student.email}</td>
+									<td className="student-old-birth-date">{student.birth_date}</td>
+									<td className="student-old-exit-level">{student.study.exit_level}</td>
+									<td className="student-old-exit-date">{student.study.exit_date}</td>
+									{hasPermission(permissions.READ_CAMPUS) && <td className="student-old-campus">{student.campus.name}</td>}
+									<td className="student-old-region">{student.region}</td>
+									<td className="student-old-action"><Link to={`/user/${student.uuid}`}>Vers le profil</Link></td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
 			)}
 		</div>
 	);
